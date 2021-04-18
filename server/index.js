c36d2971219917f6dc84bb4c87c33874feb4856a
Kelsey Myers <kelseymyerss@gmail.com>
2021-04-18 17:35:55 +0100

    deployment

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 
 import buzzRoutes from "./routes/buzzes.js";
 
 const app = express();
+dotenv.config();
 
 app.use(
   bodyParser.json({
@@ -22,13 +24,13 @@ app.use(
 app.use(cors());
 app.use("/buzzes", buzzRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://kelseymyers:Gouenji1@cluster0.i8wzc.gcp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
   })
